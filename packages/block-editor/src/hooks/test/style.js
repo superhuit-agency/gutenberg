bb5b6f2455b832e1a931b5125f2b44c80d1b85d6
Bernie Reiter <ockham@raz.or.at>
2021-05-13 21:18:23 +0200

    Oh boy

--- packages/block-editor/src/hooks/test/style.js
+++ packages/block-editor/src/hooks/test/style.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import { getInlineStyles, omitKeysNotToSerialize } from '../style';
+import { getInlineStyles } from '../style';
 
 describe( 'getInlineStyles', () => {
 	it( 'should return an empty object when called with undefined', () => {
@@ -33,34 +33,6 @@ describe( 'getInlineStyles', () => {
 			color: 'red',
 			lineHeight: 1.5,
 			fontSize: 10,
-		} );
-	} );
-} );
-
-describe( 'omitKeysNotToSerialize', () => {
-	it( 'should return the same style if no keys are skipped from serialization', () => {
-		const style = {
-			color: { text: 'red' },
-			lineHeight: 2,
-		};
-		expect( omitKeysNotToSerialize( style, {} ) ).toEqual( {
-			color: { text: 'red' },
-			lineHeight: 2,
-		} );
-	} );
-
-	it( 'should omit the color key if it is skipped for serialization', () => {
-		const style = {
-			color: { text: 'red' },
-			lineHeight: 2,
-		};
-		const blockSupports = {
-			color: {
-				__experimentalSkipSerialization: true,
-			},
-		};
-		expect( omitKeysNotToSerialize( style, blockSupports ) ).toEqual( {
-			lineHeight: 2,
 		} );
 	} );
 } );
